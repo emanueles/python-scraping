524d398e5b92d4e97fb897114bca0acf512a70bd
Emanuele Santos <emanuelemarques@gmail.com>
2015-12-05 20:58:22 -0300

    Started on chapter 3.

--- chapter3/1-getWikiLinks.py
+++ chapter3/1-getWikiLinks.py
@@ -1,4 +1,4 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 import datetime
 import random
@@ -6,10 +6,11 @@ import re
 
 random.seed(datetime.datetime.now())
 def getLinks(articleUrl):
-    html = urlopen("http://en.wikipedia.org"+articleUrl)
-    bsObj = BeautifulSoup(html)
+    html = requests.get("http://en.wikipedia.org"+articleUrl)
+    bsObj = BeautifulSoup(html.content, "html.parser")
     return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 links = getLinks("/wiki/Kevin_Bacon")
+
 while len(links) > 0:
     newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
     print(newArticle)
